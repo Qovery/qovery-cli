7967f1f3c698aa6b0e0a743a639d1cc816a3abd2
Romaric Philogene <evoxmusic@gmail.com>
2019-12-20 16:26:38 +0100

    change: auth URL

--- cmd/auth.go
+++ cmd/auth.go
@@ -18,8 +18,7 @@ var authCmd = &cobra.Command{
 	qovery auth`,
 	Run: func(cmd *cobra.Command, args []string) {
 		// TODO link to web auth
-		_ = browser.OpenURL("https://auth.qovery.com/login?state=g6Fo2SBVenl5X1dwYjFsV0dtaE03aHJmMTVfdl95RnhUNVVDbKN0aWTZIDFQWVhLRWNkS2xfUUx" +
-			"pUWY3enFVN1BYY3paX2pDNHJOo2NpZNkgTUoyU0pwdTEyUHhJemdtYzV6NVk3TjhtNU1uYUY3WTA&client=MJ2SJpu12PxIzgmc5z5Y7N8m5MnaF7Y0" +
+		_ = browser.OpenURL("https://auth.qovery.com/login?client=MJ2SJpu12PxIzgmc5z5Y7N8m5MnaF7Y0" +
 			"&protocol=oauth2&response_type=id_token%20token&audience=https://core.qovery.com&redirect_uri=" +
 			"http%3A%2F%2Flocalhost:10999/authorization")
 
