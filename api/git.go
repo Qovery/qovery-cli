668f97523ef9faa64e6ab511cf34e86c4d703b93
Romaric Philogene <evoxmusic@gmail.com>
2020-05-04 18:47:35 +0200

    feat: qovery repo check

---
+++ api/git.go
@@ -0,0 +1,49 @@
+package api
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+type GitAccessStatus struct {
+	HasAccess       bool   `json:"has_access"`
+	Message         string `json:"message"`
+	GitURL          string `json:"git_url"`
+	SanitizedGitURL string `json:"sanitized_git_url"`
+}
+
+func GitCheck(gitURL string) GitAccessStatus {
+	gas := GitAccessStatus{}
+
+	if gitURL == "" {
+		return gas
+	}
+
+	CheckAuthenticationOrQuitWithMessage()
+
+	req, _ := http.NewRequest(http.MethodGet, RootURL+"/git/access/check?url"+gitURL, nil)
+
+	req.Header.Set(headerAuthorization, headerValueBearer+GetAuthorizationToken())
+
+	client := http.Client{}
+	resp, err := client.Do(req)
+
+	if err != nil {
+		return gas
+	}
+
+	err = CheckHTTPResponse(resp)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	body, _ := ioutil.ReadAll(resp.Body)
+
+	_ = json.Unmarshal(body, &gas)
+
+	return gas
+}
--- cmd/validate.go
+++ cmd/validate.go
@@ -1,22 +1,36 @@
 package cmd
 
 import (
+	"github.com/fatih/color"
 	"github.com/spf13/cobra"
 	"os"
+	"qovery.go/api"
 	"qovery.go/util"
 )
 
 var validateCmd = &cobra.Command{
-	Use:   "validate",
+	Use:     "validate",
 	Aliases: []string{"valid"},
-	Short: "Validate the current config is valid",
-	Long: `Validate the Dockerfile and Qovery configuration file`,
+	Short:   "Validate the current config is valid",
+	Long:    `Validate the Dockerfile and Qovery configuration file`,
 	Run: func(cmd *cobra.Command, args []string) {
 		_, err := util.CurrentQoveryYML()
 		if err != nil {
 			util.PrintError("No qovery configuration file found")
 			os.Exit(1)
 		}
+
+		for _, url := range util.ListRemoteURLs() {
+			gas := api.GitCheck(url)
+
+			if gas.HasAccess {
+				println(color.GreenString("Access to " + gas.GitURL + " : OK"))
+
+			} else {
+				println(color.RedString("Access to " + gas.GitURL + " : KO"))
+			}
+		}
+
 		println("Your configuration is valid")
 	},
 }
