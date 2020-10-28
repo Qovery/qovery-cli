c30c31c26965c8f82cfc6456583eef55fb6a67fb
Patryk Jeziorowski <patryk.jeziorowski@gmx.com>
2020-10-28 13:48:52 +0100

    feat: admin options

--- cmd/admin_user.go
+++ cmd/admin_user.go
@@ -7,7 +7,10 @@ import (
 	"github.com/dgrijalva/jwt-go"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
+	"io/ioutil"
+	"net/http"
 	"qovery.go/io"
+	"strings"
 )
 
 var adminUserCmd = &cobra.Command{Use: "user"}
@@ -18,31 +21,64 @@ func init() {
 
 func getTokens() (string, string) {
 	authorizationToken := io.GetAuthorizationToken()
-	adminToken := io.GetAdminToken()
 	if authorizationToken == "" {
 		log.Fatal("Authorization token not found. Use 'qovery auth' to sign in first. ")
 	}
+
+	adminToken := getAdminToken(authorizationToken)
 	if adminToken == "" {
 		log.Fatal("Admin token not found. Use 'qovery auth' to sign as admin user first. ")
 	}
+
 	return authorizationToken, adminToken
+}
+
+func getAdminToken(authorizationToken string) string {
+	type Response struct {
+		AccessToken string `json:"access_token"`
+	}
+
+	req, _ := http.NewRequest(http.MethodGet, io.RootURL+"/admin/management-token", nil)
+	req.Header.Set("Authorization", "Bearer"+strings.TrimSpace(authorizationToken))
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var parsedResBody Response
+	err = json.Unmarshal(body, &parsedResBody)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return parsedResBody.AccessToken
 }
 
 func prepareUserMetadataPayload(user string) *bytes.Buffer {
 	payload := payload{UserMetadata: userMetadata{Rsub: user}}
+
 	jsonPayload, err := json.Marshal(payload)
 	if err != nil {
 		log.Fatal("Error preparing request body. ")
 	}
+
 	return bytes.NewBuffer(jsonPayload)
 }
 
 func prepareEmptyUserMetadataPayload() *bytes.Buffer {
 	payload := clearUserMetadataPayload{UserMetadata: emptyUserMetadata{}}
+
 	jsonPayload, err := json.Marshal(payload)
 	if err != nil {
 		log.Fatal("Error preparing request body. ")
 	}
+
 	return bytes.NewBuffer(jsonPayload)
 }
 
@@ -51,11 +87,13 @@ func getSubClaim(authorizationToken string) string {
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	var sub string
 	if claims, ok := parsed.Claims.(jwt.MapClaims); ok {
 		sub = fmt.Sprintf("%s", claims["sub"])
 	} else {
 		log.Fatal(err)
 	}
+
 	return sub
 }
