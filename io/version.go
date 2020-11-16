2d3f3ec7899a17e0217f1f7fc4b7d90eadd78fb8
Patryk Jeziorowski <patryk.jeziorowski@gmx.com>
2020-11-16 11:13:55 +0800

    fix: omit user private organization

--- io/version.go
+++ io/version.go
@@ -9,7 +9,7 @@ import (
 )
 
 func GetCurrentVersion() string {
-	return "0.32.10" // ci-version-check
+	return "0.32.11" // ci-version-check
 }
 
 func GetLatestOnlineVersionUrl() (string, error) {
