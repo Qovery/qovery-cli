3137fc07007b3be2086e906118b694d9e3bf2b92
Patryk Jeziorowski <patryk.jeziorowski@gmx.com>
2020-11-24 07:24:50 +0800

    feat: env log

--- io/version.go
+++ io/version.go
@@ -9,7 +9,7 @@ import (
 )
 
 func GetCurrentVersion() string {
-	return "0.32.12" // ci-version-check
+	return "0.32.13" // ci-version-check
 }
 
 func GetLatestOnlineVersionUrl() (string, error) {
