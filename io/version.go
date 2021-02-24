b24042b3903a5d683164c9e88e826e4b802af1f7
Pierre Mavro <pmavro@qovery.com>
2021-02-24 23:08:50 +0100

    feat: upgrade go version to 1.16

--- io/version.go
+++ io/version.go
@@ -9,7 +9,7 @@ import (
 )
 
 func GetCurrentVersion() string {
-	return "0.33.11" // ci-version-check
+	return "0.33.12" // ci-version-check
 }
 
 func GetLatestOnlineVersionUrl() (string, error) {
