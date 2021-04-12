8f4d784bbbe694de2a151d876e2c86cbcff93ba2
Pierre Mavro <pmavro@qovery.com>
2021-04-12 12:14:20 +0200

    feat: bump v0.33.20

--- io/version.go
+++ io/version.go
@@ -9,7 +9,7 @@ import (
 )
 
 func GetCurrentVersion() string {
-	return "0.33.19" // ci-version-check
+	return "0.33.20" // ci-version-check
 }
 
 func GetLatestOnlineVersionUrl() (string, error) {
