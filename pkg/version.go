54a2135c58db1cace3c01375304cdc3e914306ab
Romain GERARD <erebe@erebe.eu>
2022-08-30 15:27:41 +0200

    bump to v0.46.0

--- pkg/version.go
+++ pkg/version.go
@@ -11,7 +11,7 @@ import (
 )
 
 func GetCurrentVersion() string {
-	return "0.45.0" // ci-version-check
+	return "0.46.0" // ci-version-check
 }
 
 func GetLatestOnlineVersionUrl() (string, error) {
