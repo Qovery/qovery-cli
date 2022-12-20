ea69438b52636fb9dc7ce2242ffe96b217f49bf1
Romaric Philogène <rphilogene@qovery.com>
2022-12-20 12:33:39 +0100

    chore: bump CLI version

--- pkg/version.go
+++ pkg/version.go
@@ -11,7 +11,7 @@ import (
 )
 
 func GetCurrentVersion() string {
-	return "0.46.7" // ci-version-check
+	return "0.47.0" // ci-version-check
 }
 
 func GetLatestOnlineVersionUrl() (string, error) {
