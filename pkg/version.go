1aa32837cfe239958a407e7975765a1ba1bd2861
Romaric Philogène <rphilogene@qovery.com>
2023-04-05 12:18:57 +0200

    chore: downgrade GLIBC lib

--- pkg/version.go
+++ pkg/version.go
@@ -11,7 +11,7 @@ import (
 )
 
 func GetCurrentVersion() string {
-	return "0.56.0" // ci-version-check
+	return "0.56.1" // ci-version-check
 }
 
 func GetLatestOnlineVersionUrl() (string, error) {
