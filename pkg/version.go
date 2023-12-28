545eb610765d1477893f421b8ed3b8c036692997
Σrebe - Romain GERARD <erebe@erebe.eu>
2023-12-28 20:42:17 +0100

    v0.78.0

--- pkg/version.go
+++ pkg/version.go
@@ -11,7 +11,7 @@ import (
 )
 
 func GetCurrentVersion() string {
-	return "0.77.0" // ci-version-check
+	return "0.78.0" // ci-version-check
 }
 
 func GetLatestOnlineVersionUrl() (string, error) {
