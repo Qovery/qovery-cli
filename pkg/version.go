65f0dfca775d427e8250783cd3a8a25150518aaf
Σrebe - Romain GERARD <erebe@erebe.eu>
2024-06-03 17:37:54 +0200

    Bump version

--- pkg/version.go
+++ pkg/version.go
@@ -11,7 +11,7 @@ import (
 )
 
 func GetCurrentVersion() string {
-	return "0.93.5" // ci-version-check
+	return "0.93.6" // ci-version-check
 }
 
 func GetLatestOnlineVersionUrl() (string, error) {
