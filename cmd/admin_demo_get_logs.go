b3d73580fed211b3cb5dada24b1a263c4557b96b
Σrebe - Romain GERARD <erebe@erebe.eu>
2024-07-22 15:50:54 +0200

    feat(demo): Add command to list and retrieve demo error logs

---
+++ cmd/admin_demo_get_logs.go
@@ -0,0 +1,66 @@
+package cmd
+
+import (
+	"bytes"
+	"fmt"
+	log "github.com/sirupsen/logrus"
+	"io"
+	"net/http"
+	"os"
+
+	"github.com/spf13/cobra"
+
+	"github.com/qovery/qovery-cli/utils"
+)
+
+type ListLogResponse struct {
+	Filename     string `json:"filename"`
+	LastModified string `json:"last_modified"`
+}
+
+var (
+	adminDemoGetLogsCmd = &cobra.Command{
+		Use:   "get-log",
+		Short: "retrieve a specific log",
+		Run: func(cmd *cobra.Command, args []string) {
+			utils.Capture(cmd)
+
+			if len(args) == 0 {
+				log.Fatal("You must specify a log filename as argument")
+				os.Exit(0)
+			}
+
+			tokenType, token, err := utils.GetAccessToken()
+			if err != nil {
+				utils.PrintlnError(err)
+				os.Exit(1)
+			}
+
+			url := fmt.Sprintf("%s/demoDebugLog", utils.AdminUrl)
+			req, _ := http.NewRequest(http.MethodGet, url, bytes.NewReader([]byte{}))
+			query := req.URL.Query()
+			query.Add("filename", args[0])
+			req.URL.RawQuery = query.Encode()
+			req.Header.Set("Authorization", utils.GetAuthorizationHeaderValue(tokenType, token))
+
+			response, err := http.DefaultClient.Do(req)
+			if err != nil {
+				utils.PrintlnError(fmt.Errorf("error uploading debug logs: %s", err))
+				return
+			}
+
+			body, _ := io.ReadAll(response.Body)
+			if response.StatusCode != http.StatusOK {
+				utils.PrintlnError(fmt.Errorf("error uploading debug logs: %s %s", response.Status, body))
+				return
+			}
+
+			_ = os.WriteFile(args[0], body, 0640)
+			log.Info("file written to ", args[0])
+		},
+	}
+)
+
+func init() {
+	adminDemoCmd.AddCommand(adminDemoGetLogsCmd)
+}
---
+++ cmd/admin_demo_list_logs.go
@@ -0,0 +1,71 @@
+package cmd
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+
+	"github.com/spf13/cobra"
+
+	"github.com/qovery/qovery-cli/utils"
+)
+
+type listLogResponse struct {
+	Filename     string `json:"filename"`
+	LastModified string `json:"last_modified"`
+}
+
+var (
+	adminDemoListLogsCmd = &cobra.Command{
+		Use:   "list-logs",
+		Short: "list error logs from the command demo up",
+		Run: func(cmd *cobra.Command, args []string) {
+			utils.Capture(cmd)
+
+			tokenType, token, err := utils.GetAccessToken()
+			if err != nil {
+				utils.PrintlnError(err)
+				os.Exit(1)
+			}
+
+			url := fmt.Sprintf("%s/demoDebugLog", utils.AdminUrl)
+			req, _ := http.NewRequest(http.MethodGet, url, bytes.NewReader([]byte{}))
+			query := req.URL.Query()
+			orgaId, _ := cmd.Flags().GetString("organizationId")
+			if orgaId != "" {
+				query.Add("organization", orgaId)
+			}
+			req.URL.RawQuery = query.Encode()
+			req.Header.Set("Authorization", utils.GetAuthorizationHeaderValue(tokenType, token))
+
+			response, err := http.DefaultClient.Do(req)
+			if err != nil {
+				utils.PrintlnError(fmt.Errorf("error uploading debug logs: %s", err))
+				return
+			}
+
+			body, _ := io.ReadAll(response.Body)
+			if response.StatusCode != http.StatusOK {
+				utils.PrintlnError(fmt.Errorf("error uploading debug logs: %s %s", response.Status, body))
+				return
+			}
+
+			var responseObject []listLogResponse
+			_ = json.Unmarshal(body, &responseObject)
+
+			var rows [][]string
+			for _, row := range responseObject {
+				rows = append(rows, []string{row.LastModified, row.Filename})
+			}
+			_ = utils.PrintTable([]string{"date", "filename"}, rows)
+		},
+	}
+)
+
+func init() {
+	adminDemoListLogsCmd.Flags().StringP("organizationId", "o", "", "Organization to filter on for listing")
+	adminDemoCmd.AddCommand(adminDemoListLogsCmd)
+}
