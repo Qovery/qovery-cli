32f6e914eae6b9ad79b2e4c8f7365b6c6f023455
Romaric P <rphilogene@qovery.com>
2024-07-23 16:40:33 +0200

    feat: add flag --services and for each service to deploy multiple services with one qovery environment deploy command

--- cmd/environment_deploy.go
+++ cmd/environment_deploy.go
@@ -7,6 +7,7 @@ import (
 	"github.com/pterm/pterm"
 	"os"
 	"slices"
+	"strings"
 	"time"
 
 	"github.com/qovery/qovery-cli/utils"
@@ -38,8 +39,10 @@ var environmentDeployCmd = &cobra.Command{
 			panic("unreachable") // staticcheck false positive: https://staticcheck.io/docs/checks#SA5011
 		}
 
-		if servicesJson != "" && skipPausedServicesFlag {
-			utils.PrintlnError(fmt.Errorf("services and skip-paused-services flags are mutually exclusive"))
+		if (servicesJson != "" || applicationNames != "" || containerNames != "" || lifecycleNames != "" ||
+			cronjobNames != "" || helmNames != "") && skipPausedServicesFlag {
+			utils.PrintlnError(fmt.Errorf("you can't use --skip-paused-services flag with --services, " +
+				"--applications, --containers, --lifecycles, --cronjobs or --helms flags"))
 			os.Exit(1)
 			panic("unreachable") // staticcheck false positive: https://staticcheck.io/docs/checks#SA5011
 		}
@@ -70,6 +73,16 @@ var environmentDeployCmd = &cobra.Command{
 				os.Exit(1)
 				panic("unreachable") // staticcheck false positive: https://staticcheck.io/docs/checks#SA5011
 			}
+			utils.Println("Services are deploying!")
+		} else if applicationNames != "" || containerNames != "" || lifecycleNames != "" || cronjobNames != "" || helmNames != "" {
+			deploymentRequest := getDeploymentRequestForMultipleServices(client, envId, applicationNames, containerNames, lifecycleNames, cronjobNames, helmNames)
+			_, _, err = client.EnvironmentActionsAPI.DeployAllServices(context.Background(), envId).DeployAllRequest(deploymentRequest).Execute()
+			if err != nil {
+				utils.PrintlnError(err)
+				os.Exit(1)
+				panic("unreachable") // staticcheck false positive: https://staticcheck.io/docs/checks#SA5011
+			}
+
 			utils.Println("Services are deploying!")
 		}
 
@@ -113,7 +126,8 @@ var environmentDeployCmd = &cobra.Command{
 				panic("unreachable") // staticcheck false positive: https://staticcheck.io/docs/checks#SA5011
 			}
 
-		} else if servicesJson == "" {
+		} else if servicesJson == "" && applicationNames == "" && containerNames == "" && lifecycleNames == "" &&
+			cronjobNames == "" && helmNames == "" {
 			// Deploy the whole env
 			_, _, err = client.EnvironmentActionsAPI.DeployEnvironment(context.Background(), envId).Execute()
 			if err != nil {
@@ -128,6 +142,157 @@ var environmentDeployCmd = &cobra.Command{
 			utils.WatchEnvironment(envId, qovery.STATEENUM_DEPLOYED, client)
 		}
 	},
+}
+
+/**
+ * Get deployment request for multiple services
+ */
+func getDeploymentRequestForMultipleServices(
+	client *qovery.APIClient,
+	envId string,
+	applicationNames string,
+	containerNames string,
+	lifecycleNames string,
+	cronjobNames string,
+	helmNames string,
+) qovery.DeployAllRequest {
+	// Deploy the services from the env
+	request := qovery.DeployAllRequest{}
+
+	if applicationNames != "" {
+		// Adding applications to be deployed
+		for _, nameAndVersion := range strings.Split(applicationNames, ",") {
+			name, version := splitServiceNameAndVersion(nameAndVersion)
+
+			apps, _, err := client.ApplicationsAPI.ListApplication(context.Background(), envId).Execute()
+			if err != nil {
+				utils.PrintlnError(err)
+				os.Exit(1)
+				panic("unreachable") // staticcheck false positive: https://staticcheck.io/docs/checks#SA5011
+			}
+
+			app := utils.FindByApplicationName(apps.GetResults(), name)
+
+			if app == nil {
+				utils.PrintlnError(fmt.Errorf("application %s not found", name))
+				os.Exit(1)
+				panic("unreachable") // staticcheck false positive: https://staticcheck.io/docs/checks#SA5011
+			}
+
+			request.Applications = append(request.Applications, qovery.DeployAllRequestApplicationsInner{ApplicationId: app.Id, GitCommitId: version})
+		}
+	}
+
+	if containerNames != "" {
+		// Adding containers to be deployed
+		for _, nameAndVersion := range strings.Split(containerNames, ",") {
+			name, version := splitServiceNameAndVersion(nameAndVersion)
+
+			containers, _, err := client.ContainersAPI.ListContainer(context.Background(), envId).Execute()
+			if err != nil {
+				utils.PrintlnError(err)
+				os.Exit(1)
+				panic("unreachable") // staticcheck false positive: https://staticcheck.io/docs/checks#SA5011
+			}
+
+			container := utils.FindByContainerName(containers.GetResults(), name)
+
+			request.Containers = append(request.Containers, qovery.DeployAllRequestContainersInner{Id: container.Id, ImageTag: version})
+		}
+	}
+
+	if lifecycleNames != "" || cronjobNames != "" {
+		jobs, _, err := client.JobsAPI.ListJobs(context.Background(), envId).Execute()
+
+		if err != nil {
+			utils.PrintlnError(err)
+			os.Exit(1)
+			panic("unreachable") // staticcheck false positive: https://staticcheck.io/docs/checks#SA5011
+		}
+
+		if lifecycleNames != "" {
+			// Adding lifecycle to be deployed
+			for _, nameAndVersion := range strings.Split(lifecycleNames, ",") {
+				name, version := splitServiceNameAndVersion(nameAndVersion)
+				job, gitCommitId, imageTag := getLifecycleJobGitCommitAndImageTag(jobs.GetResults(), name)
+
+				if job == nil {
+					utils.PrintlnError(fmt.Errorf("lifecycle %s not found", name))
+					os.Exit(1)
+					panic("unreachable") // staticcheck false positive: https://staticcheck.io/docs/checks#SA5011
+				}
+
+				req := qovery.DeployAllRequestJobsInner{Id: &job.LifecycleJobResponse.Id}
+				if gitCommitId != nil {
+					req.GitCommitId = version
+				} else if imageTag != nil {
+					req.ImageTag = version
+				}
+
+				request.Jobs = append(request.Jobs, req)
+			}
+		}
+
+		if cronjobNames != "" {
+			// Adding cronjobs to be deployed
+			for _, nameAndVersion := range strings.Split(cronjobNames, ",") {
+				name, version := splitServiceNameAndVersion(nameAndVersion)
+
+				job, gitCommitId, imageTag := getCronjobGitCommitAndImageTag(jobs.GetResults(), name)
+
+				if job == nil {
+					utils.PrintlnError(fmt.Errorf("cronjob %s not found", name))
+					os.Exit(1)
+					panic("unreachable") // staticcheck false positive: https://staticcheck.io/docs/checks#SA5011
+				}
+
+				req := qovery.DeployAllRequestJobsInner{Id: &job.CronJobResponse.Id}
+				if gitCommitId != nil {
+					req.GitCommitId = version
+				} else if imageTag != nil {
+					req.ImageTag = version
+				}
+
+				request.Jobs = append(request.Jobs, req)
+			}
+		}
+	}
+
+	if helmNames != "" {
+		// Adding helms to be deployed
+		for _, nameAndVersion := range strings.Split(helmNames, ",") {
+			name, version := splitServiceNameAndVersion(nameAndVersion)
+
+			helms, _, err := client.HelmsAPI.ListHelms(context.Background(), envId).Execute()
+
+			if err != nil {
+				utils.PrintlnError(err)
+				os.Exit(1)
+				panic("unreachable") // staticcheck false positive: https://staticcheck.io/docs/checks#SA5011
+			}
+
+			helm := utils.FindByHelmName(helms.GetResults(), name)
+
+			if helm == nil {
+				utils.PrintlnError(fmt.Errorf("helm %s not found", name))
+				os.Exit(1)
+				panic("unreachable") // staticcheck false positive: https://staticcheck.io/docs/checks#SA5011
+			}
+
+			gitCommitId, chartVersion := getHelmCommitAndChartVersion(client, name)
+
+			req := qovery.DeployAllRequestHelmsInner{Id: &helm.Id}
+			if gitCommitId != nil {
+				req.GitCommitId = version
+			} else if chartVersion != nil {
+				req.ChartVersion = version
+			}
+
+			request.Helms = append(request.Helms, req)
+		}
+	}
+
+	return request
 }
 
 type Services struct {
@@ -151,7 +316,7 @@ func getEligibleServices(client *qovery.APIClient, envId string, servicesStatuse
 		return nonStoppedServices, err
 	}
 
-	// Gather all non stopped services
+	// Gather all non-stopped services
 	for _, serviceStatus := range envStatuses.Applications {
 		if !slices.Contains(servicesStatusesToExclude, serviceStatus.GetState()) {
 			nonStoppedServices.ApplicationsIDs = append(nonStoppedServices.ApplicationsIDs, serviceStatus.Id)
@@ -181,12 +346,98 @@ func getEligibleServices(client *qovery.APIClient, envId string, servicesStatuse
 	return nonStoppedServices, nil
 }
 
+/**
+ * Split service name and version (if provided)
+ */
+func splitServiceNameAndVersion(service string) (string, *string) {
+	split := strings.Split(service, ":")
+	if len(split) == 1 {
+		return split[0], nil
+	}
+
+	return split[0], &split[1]
+}
+
+func getLifecycleJobGitCommitAndImageTag(jobs []qovery.JobResponse, jobName string) (*qovery.JobResponse, *string, *string) {
+	var commitId, imageTag *string
+
+	job := utils.FindByJobName(jobs, jobName)
+
+	if job == nil {
+		return nil, nil, nil
+	}
+
+	if job.LifecycleJobResponse.Source.BaseJobResponseAllOfSourceOneOf != nil {
+		// image tag
+		image := job.LifecycleJobResponse.Source.BaseJobResponseAllOfSourceOneOf.GetImage()
+		tag := image.GetTag()
+		imageTag = &tag
+	} else if job.LifecycleJobResponse.Source.BaseJobResponseAllOfSourceOneOf1 != nil {
+		// commit id
+		docker := job.LifecycleJobResponse.Source.BaseJobResponseAllOfSourceOneOf1.GetDocker()
+		commitId = docker.GitRepository.DeployedCommitId
+	}
+
+	return job, commitId, imageTag
+}
+
+func getCronjobGitCommitAndImageTag(jobs []qovery.JobResponse, jobName string) (*qovery.JobResponse, *string, *string) {
+	var commitId, imageTag *string
+
+	job := utils.FindByJobName(jobs, jobName)
+
+	if job == nil {
+		return nil, nil, nil
+	}
+
+	if job.CronJobResponse.Source.BaseJobResponseAllOfSourceOneOf != nil {
+		// image tag
+		image := job.CronJobResponse.Source.BaseJobResponseAllOfSourceOneOf.GetImage()
+		tag := image.GetTag()
+		imageTag = &tag
+	} else if job.CronJobResponse.Source.BaseJobResponseAllOfSourceOneOf1 != nil {
+		// commit id
+		docker := job.CronJobResponse.Source.BaseJobResponseAllOfSourceOneOf1.GetDocker()
+		commitId = docker.GitRepository.DeployedCommitId
+	}
+
+	return job, commitId, imageTag
+}
+
+func getHelmCommitAndChartVersion(client *qovery.APIClient, helmId string) (*string, *string) {
+	var commitId, chartVersion *string
+
+	// check if the helm version is a chart version or a commit id
+	helm, _, err := client.HelmMainCallsAPI.GetHelm(context.Background(), helmId).Execute()
+	if err != nil {
+		utils.PrintlnError(err)
+		os.Exit(1)
+	}
+
+	if helm.Source.HelmResponseAllOfSourceOneOf != nil {
+		// chart version
+		git := helm.Source.HelmResponseAllOfSourceOneOf.GetGit()
+		commitId = git.GitRepository.DeployedCommitId
+	} else if helm.Source.HelmResponseAllOfSourceOneOf1 != nil {
+		// commit id
+		git := helm.Source.HelmResponseAllOfSourceOneOf1.GetRepository()
+		chartVersion = &git.ChartVersion
+	}
+
+	return commitId, chartVersion
+}
+
 func init() {
 	environmentCmd.AddCommand(environmentDeployCmd)
 	environmentDeployCmd.Flags().StringVarP(&organizationName, "organization", "", "", "Organization Name")
 	environmentDeployCmd.Flags().StringVarP(&projectName, "project", "", "", "Project Name")
 	environmentDeployCmd.Flags().StringVarP(&environmentName, "environment", "", "", "Environment Name")
 	environmentDeployCmd.Flags().StringVarP(&servicesJson, "services", "", "", "Services to deploy (JSON Format: https://api-doc.qovery.com/#tag/Environment-Actions/operation/deployAllServices)")
+	environmentDeployCmd.Flags().StringVarP(&applicationNames, "applications", "", "", "Applications to deploy E.g. --applications app1:commit_id,app2:commit_id). If you omit the commit id, the same commit will be used")
+	environmentDeployCmd.Flags().StringVarP(&containerNames, "containers", "", "", "Containers to deploy E.g. --containers container1:image_tag,container2:image_tag). If you omit the image tag, the same image tag will be used")
+	environmentDeployCmd.Flags().StringVarP(&lifecycleNames, "lifecycles", "", "", "Lifecycle to deploy E.g. --lifecycles job1:image_tag|git_commit_id,job2:image_tag|git_commit_id). If you omit the git commit id or image tag, the same version will be used")
+	environmentDeployCmd.Flags().StringVarP(&cronjobNames, "cronjobs", "", "", "Cronjobs to deploy E.g. --cronjobs cronjob1:git_commit_id,cronjob2:git_commit_id). If you omit the git commit id, the same version will be used")
+	environmentDeployCmd.Flags().StringVarP(&helmNames, "helms", "", "", "Helms to deploy E.g. --helms helm1:chart_version|git_commit_id,helm2:chart_version|git_commit_id). If you omit the chart version or git commit id, the same version will be used")
 	environmentDeployCmd.Flags().BoolVarP(&watchFlag, "watch", "w", false, "Watch environment status until it's ready or an error occurs")
 	environmentDeployCmd.Flags().BoolVarP(&skipPausedServicesFlag, "skip-paused-services", "", false, "Skip paused services: paused services won't be started / deployed")
 }
