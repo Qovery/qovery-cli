d2db8bbcc60d38bcc0f1e80d212dd59c0b3d746d
Fabien FLEUREAU <ffleureau@qovery.com>
2025-03-05 16:46:57 +0100

    feat: update helm deployment actions

--- cmd/helm_stop.go
+++ cmd/helm_stop.go
@@ -3,13 +3,11 @@ package cmd
 import (
 	"context"
 	"fmt"
+	"github.com/pterm/pterm"
 	"github.com/qovery/qovery-client-go"
+	"github.com/spf13/cobra"
 	"os"
 	"strings"
-	"time"
-
-	"github.com/pterm/pterm"
-	"github.com/spf13/cobra"
 
 	"github.com/qovery/qovery-cli/utils"
 )
@@ -20,121 +18,22 @@ var helmStopCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		utils.Capture(cmd)
 
-		tokenType, token, err := utils.GetAccessToken()
-		checkError(err)
-
+		client := utils.GetQoveryClientPanicInCaseOfError()
 		validateHelmArguments(helmName, helmNames)
+		envId := getEnvironmentIdFromContextPanicInCaseOfError(client)
 
-		client := utils.GetQoveryClient(tokenType, token)
-		organizationId, _, envId, err := getOrganizationProjectEnvironmentContextResourcesIds(client)
-		checkError(err)
-
-		if isDeploymentQueueEnabledForOrganization(organizationId) {
-			serviceIds := utils.Map(buildHelmListFromHelmNames(client, envId, helmName, helmNames),
-				func(helm *qovery.HelmResponse) string {
+		helmList := buildHelmListFromHelmNames(client, envId, helmName, helmNames)
+		_, err := client.EnvironmentActionsAPI.
+			StopSelectedServices(context.Background(), envId).
+			EnvironmentServiceIdsAllRequest(qovery.EnvironmentServiceIdsAllRequest{
+				HelmIds: utils.Map(helmList, func(helm *qovery.HelmResponse) string {
 					return helm.Id
-				})
-			_, err := client.EnvironmentActionsAPI.
-				StopSelectedServices(context.Background(), envId).
-				EnvironmentServiceIdsAllRequest(qovery.EnvironmentServiceIdsAllRequest{
-					HelmIds: serviceIds,
-				}).
-				Execute()
-			checkError(err)
-			utils.Println(fmt.Sprintf("Request to stop helm(s) %s has been queued...", pterm.FgBlue.Sprintf("%s%s", helmName, helmNames)))
-			if watchFlag {
-				utils.WatchEnvironment(envId, "unused", client)
-			}
-			return
-		}
-
-		// TODO(ENG-1883) once deployment queue is enabled for all organizations, remove the following code block
-		if helmNames != "" {
-			// wait until service is ready
-			for {
-				if utils.IsEnvironmentInATerminalState(envId, client) {
-					break
-				}
-
-				utils.Println(fmt.Sprintf("Waiting for environment %s to be ready..", pterm.FgBlue.Sprintf("%s", envId)))
-				time.Sleep(5 * time.Second)
-			}
-
-			helms, _, err := client.HelmsAPI.ListHelms(context.Background(), envId).Execute()
-
-			if err != nil {
-				utils.PrintlnError(err)
-				os.Exit(1)
-				panic("unreachable") // staticcheck false positive: https://staticcheck.io/docs/checks#SA5011
-			}
-
-			var serviceIds []string
-			for _, helmName := range strings.Split(helmNames, ",") {
-				trimmedHelmName := strings.TrimSpace(helmName)
-
-				helm := utils.FindByHelmName(helms.GetResults(), trimmedHelmName)
-				if helm == nil {
-					utils.PrintlnError(fmt.Errorf("helm %s not found", trimmedHelmName))
-					utils.PrintlnInfo("You can list all helms with: qovery helm list")
-					os.Exit(1)
-					panic("unreachable") // staticcheck false positive: https://staticcheck.io/docs/checks#SA5011
-				}
-				serviceIds = append(serviceIds, helm.Id)
-			}
-
-			// stop multiple services
-			_, err = utils.StopServices(client, envId, serviceIds, utils.HelmType)
-
-			if watchFlag {
-				utils.WatchEnvironment(envId, qovery.STATEENUM_STOPPED, client)
-			} else {
-				utils.Println(fmt.Sprintf("Stopping helms %s in progress..", pterm.FgBlue.Sprintf("%s", helmNames)))
-			}
-
-			if err != nil {
-				utils.PrintlnError(err)
-				os.Exit(1)
-				panic("unreachable") // staticcheck false positive: https://staticcheck.io/docs/checks#SA5011
-			}
-
-			return
-		}
-
-		helms, _, err := client.HelmsAPI.ListHelms(context.Background(), envId).Execute()
-
-		if err != nil {
-			utils.PrintlnError(err)
-			os.Exit(1)
-			panic("unreachable") // staticcheck false positive: https://staticcheck.io/docs/checks#SA5011
-		}
-
-		helm := utils.FindByHelmName(helms.GetResults(), helmName)
-
-		if helm == nil {
-			utils.PrintlnError(fmt.Errorf("helm %s not found", helmName))
-			utils.PrintlnInfo("You can list all helms with: qovery helm list")
-			os.Exit(1)
-			panic("unreachable") // staticcheck false positive: https://staticcheck.io/docs/checks#SA5011
-		}
-
-		msg, err := utils.StopService(client, envId, helm.Id, utils.HelmType, watchFlag)
-
-		if err != nil {
-			utils.PrintlnError(err)
-			os.Exit(1)
-			panic("unreachable") // staticcheck false positive: https://staticcheck.io/docs/checks#SA5011
-		}
-
-		if msg != "" {
-			utils.PrintlnInfo(msg)
-			return
-		}
-
-		if watchFlag {
-			utils.Println(fmt.Sprintf("Helm %s stopped!", pterm.FgBlue.Sprintf("%s", helmName)))
-		} else {
-			utils.Println(fmt.Sprintf("Stopping helm %s in progress..", pterm.FgBlue.Sprintf("%s", helmName)))
-		}
+				}),
+			}).
+			Execute()
+		checkError(err)
+		utils.Println(fmt.Sprintf("Request to stop helm(s) %s has been queued...", pterm.FgBlue.Sprintf("%s%s", helmName, helmNames)))
+		WatchHelmDeployment(client, envId, helmList, watchFlag, qovery.STATEENUM_STOPPED)
 	},
 }
 
