b249d4ff0a325c507ceadcc06f1912fd8d0fb8cc
Σrebe - Romain GERARD <erebe@erebe.eu>
2025-05-13 14:44:51 +0200

    chore(sec): replace vulnerable and deprecated lib
    https://github.com/Qovery/qovery-cli/security/dependabot/51

--- cmd/upgrade.go
+++ cmd/upgrade.go
@@ -4,20 +4,20 @@
 package cmd
 
 import (
+	"context"
 	"fmt"
 	"github.com/kardianos/osext"
-	"github.com/mholt/archiver/v3"
+	"github.com/mholt/archives"
 	"github.com/qovery/qovery-cli/pkg"
 	"github.com/qovery/qovery-cli/utils"
 	"github.com/spf13/cobra"
 	"golang.org/x/sys/unix"
+	"io"
 	"net/http"
 	"os"
 	"os/exec"
 	"runtime"
 )
-
-import iio "io"
 
 var upgradeCmd = &cobra.Command{
 	Use:   "upgrade",
@@ -30,7 +30,7 @@ var upgradeCmd = &cobra.Command{
 		archivePath := "/tmp/"
 		archiveName := filename + ".tgz"
 		archivePathName := archivePath + archiveName
-		uncompressPath := "/tmp/" + filename + "/"
+		uncompressPath := "/tmp/"
 		uncompressQoveryBinaryPath := uncompressPath + filename
 		cleanList := []string{uncompressPath, archivePathName}
 
@@ -40,50 +40,76 @@ var upgradeCmd = &cobra.Command{
 			os.Exit(0)
 		}
 
-		url := fmt.Sprintf("https://github.com/Qovery/qovery-cli/releases/download/v%s/qovery-cli_%s_%s_%s.tar.gz",
-			desiredVersion, desiredVersion, runtime.GOOS, runtime.GOARCH)
+		urlFilename := fmt.Sprintf("qovery-cli_%s_%s_%s.tar.gz", desiredVersion, runtime.GOOS, runtime.GOARCH)
+		url := fmt.Sprintf("https://github.com/Qovery/qovery-cli/releases/download/v%s/%s", desiredVersion, urlFilename)
 
 		binaryWriteAccess := unix.Access(currentBinaryFilename, unix.W_OK)
 		if binaryWriteAccess != nil {
-			utils.PrintlnError(fmt.Errorf("Upgrade cancelled: no write permission on the Qovery CLI binary file: %s", currentBinaryFilename))
+			utils.PrintlnError(fmt.Errorf("upgrade cancelled: no write permission on the Qovery CLI binary file: %s", currentBinaryFilename))
 			cleanArchives(cleanList)
 			os.Exit(0)
 		}
 
 		resp, err := http.Get(url)
 		if err != nil {
-			utils.PrintlnError(fmt.Errorf("Error while downloading the latest version: %s", err))
+			utils.PrintlnError(fmt.Errorf("error while downloading the latest version: %s", err))
 			os.Exit(0)
 		}
 		defer resp.Body.Close()
 
 		out, err := os.Create(archivePathName)
 		if err != nil {
-			utils.PrintlnError(fmt.Errorf("Error while overriding Qovery CLI binary file: %s", err))
+			utils.PrintlnError(fmt.Errorf("error while overriding Qovery CLI binary file: %s", err))
 			os.Exit(0)
 		}
 		defer out.Close()
-
-		_, err = iio.Copy(out, resp.Body)
-		if err != nil {
-			utils.PrintlnError(fmt.Errorf("Error while adding content to Qovery CLI binary file: %s", err))
-			os.Exit(0)
-		}
 
 		if _, err := os.Stat(uncompressPath); !os.IsNotExist(err) {
 			os.RemoveAll(uncompressPath)
 		}
 
-		err = archiver.Unarchive(archivePathName, uncompressPath)
+		// Decompress the tar.gz and extract the cli
+		format, stream, err := archives.Identify(context.Background(), urlFilename, resp.Body)
 		if err != nil {
-			utils.PrintlnError(fmt.Errorf("Error while uncompressing the archive: %s", err))
+			utils.PrintlnError(fmt.Errorf("cannot identify archive format: %s", err))
 			os.Exit(0)
 		}
 
-		// Fork to avoid override issue on a a running program
+		if ex, ok := format.(archives.Extractor); ok {
+
+			// function that will be called for every file inside the archive.
+			// archives.FileInfo is going to contain the file info inside the archive
+			err = ex.Extract(context.Background(), stream, func(ctx context.Context, f archives.FileInfo) error {
+				if f.NameInArchive != "qovery" {
+					return nil
+				}
+
+				// Extract the cli from the archive on disk
+				cliFileInsideArchive, _ := f.Open()
+				defer cliFileInsideArchive.Close()
+
+				cliFileOnFS, _ := os.Create(uncompressQoveryBinaryPath)
+				defer cliFileOnFS.Close()
+
+				_, err = io.Copy(cliFileOnFS, cliFileInsideArchive)
+				if err != nil {
+					utils.PrintlnError(fmt.Errorf("error while uncompressing the cli on disk: %s", err))
+					os.Exit(0)
+				}
+
+				_ = cliFileOnFS.Chmod(0555)
+				return nil
+			})
+		}
+
+		if err != nil {
+			utils.PrintlnError(fmt.Errorf("error while uncompressing the archive: %s", err))
+			os.Exit(0)
+		}
+
+		// Fork to avoid override issue on a running program
 		utils.PrintlnInfo(fmt.Sprintf("\nUpgrading Qovery CLI to version %s\n", desiredVersion))
-		command := exec.Command("/bin/sh", "-c", "sleep 1 ; mv "+uncompressQoveryBinaryPath+" "+
-			currentBinaryFilename)
+		command := exec.Command("/bin/sh", "-c", "mv "+uncompressQoveryBinaryPath+" "+currentBinaryFilename)
 		err = command.Start()
 		if err != nil {
 			utils.PrintlnError(err)
@@ -100,7 +126,7 @@ func cleanArchives(listToRemove []string) {
 	for _, value := range listToRemove {
 		err := os.RemoveAll(value)
 		if err != nil {
-			utils.PrintlnError(fmt.Errorf("Error while removing the element: %s", err))
+			utils.PrintlnError(fmt.Errorf("error while removing the element: %s", err))
 			os.Exit(0)
 		}
 	}
--- go.mod
+++ go.mod
@@ -19,20 +19,20 @@ require (
 	github.com/joho/godotenv v1.5.1
 	github.com/kardianos/osext v0.0.0-20190222173326-2bc1f35cddc0
 	github.com/manifoldco/promptui v0.9.0
-	github.com/mholt/archiver/v3 v3.5.1
+	github.com/mholt/archives v0.1.1
 	github.com/pkg/browser v0.0.0-20240102092130-5ac0b6a4141c
 	github.com/pkg/errors v0.9.1
-	github.com/posthog/posthog-go v1.4.10
+	github.com/posthog/posthog-go v1.5.2
 	github.com/pterm/pterm v0.12.80
-	github.com/qovery/qovery-client-go v0.0.0-20250428130721-f0ed655a70bb
+	github.com/qovery/qovery-client-go v0.0.0-20250513112133-583c70b9397f
 	github.com/sirupsen/logrus v1.9.3
 	github.com/spf13/cobra v1.9.1
 	github.com/spf13/pflag v1.0.6
 	github.com/stretchr/testify v1.10.0
 	github.com/tonistiigi/go-rosetta v0.0.0-20220804170347-3f4430f2d346
 	github.com/xlab/treeprint v1.2.0
-	golang.org/x/net v0.39.0
-	golang.org/x/sys v0.32.0
+	golang.org/x/net v0.40.0
+	golang.org/x/sys v0.33.0
 	gopkg.in/yaml.v3 v3.0.1
 )
 
@@ -40,12 +40,16 @@ require (
 	atomicgo.dev/cursor v0.2.0 // indirect
 	atomicgo.dev/keyboard v0.2.9 // indirect
 	atomicgo.dev/schedule v0.1.0 // indirect
+	github.com/STARRY-S/zip v0.2.3 // indirect
 	github.com/andybalholm/brotli v1.1.1 // indirect
+	github.com/bodgit/plumbing v1.3.0 // indirect
+	github.com/bodgit/sevenzip v1.6.1 // indirect
+	github.com/bodgit/windows v1.0.1 // indirect
 	github.com/chzyer/readline v1.5.1 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/dsnet/compress v0.0.2-0.20210315054119-f66993602bf5 // indirect
-	github.com/golang/snappy v1.0.0 // indirect
+	github.com/dsnet/compress v0.0.2-0.20230904184137-39efe44ab707 // indirect
 	github.com/gookit/color v1.5.4 // indirect
+	github.com/hashicorp/golang-lru/v2 v2.0.7 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/kballard/go-shellquote v0.0.0-20180428030007-95032a82bc51 // indirect
 	github.com/klauspost/compress v1.18.0 // indirect
@@ -55,13 +59,17 @@ require (
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-runewidth v0.0.16 // indirect
 	github.com/mgutz/ansi v0.0.0-20200706080929-d51e80ef957d // indirect
-	github.com/nwaples/rardecode v1.1.3 // indirect
+	github.com/minio/minlz v1.0.1 // indirect
+	github.com/nwaples/rardecode/v2 v2.1.1 // indirect
 	github.com/pierrec/lz4/v4 v4.1.22 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
+	github.com/sorairolake/lzip-go v0.3.7 // indirect
+	github.com/spf13/afero v1.14.0 // indirect
+	github.com/therootcompany/xz v1.0.1 // indirect
 	github.com/ulikunitz/xz v0.5.12 // indirect
-	github.com/xi2/xz v0.0.0-20171230120015-48954b6210f8 // indirect
 	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
-	golang.org/x/term v0.31.0 // indirect
-	golang.org/x/text v0.24.0 // indirect
+	go4.org v0.0.0-20230225012048-214862532bf5 // indirect
+	golang.org/x/term v0.32.0 // indirect
+	golang.org/x/text v0.25.0 // indirect
 )
