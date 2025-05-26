d2edec6284dbb48b520edd06490129ad4ae28deb
Fabien FLEUREAU <ffleureau@qovery.com>
2025-05-26 17:37:33 +0200

    feat(QOV-799): add command to load AWS credentials from a role ARN

--- cmd/admin_load_aws_credentials.go
+++ cmd/admin_load_aws_credentials.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"github.com/qovery/qovery-cli/utils"
 	"github.com/spf13/cobra"
 
 	"github.com/qovery/qovery-cli/pkg"
@@ -14,17 +15,18 @@ var (
 		Long: `This command is used to load aws credentials 
 > Examples
 ----------
-* Load AWS credentials from a role ARN arn:aws:iam::123456789012:role/qovery
-qovery admin load-aws-credentials --role-arn arn:aws:iam::123456789012:role/qovery
+* Load AWS credentials from a role ARN arn:aws:iam::123456789012:role/qovery-user-role-xxx
+qovery admin load-aws-credentials --role-arn arn:aws:iam::123456789012:role/qovery-user-role-xxx
 
 `,
 		Run: func(cmd *cobra.Command, args []string) {
-			pkg.LoadAwsCredentials(roleArn)
+			err := pkg.LoadAwsCredentials(roleArn)
+			utils.CheckError(err)
 		},
 	}
 )
 
 func init() {
 	adminLoadAwsCredentialsCmd.Flags().StringVarP(&roleArn, "role-arn", "r", "", "ARN of the AWS IAM role to assume")
-	adminClusterCmd.AddCommand(adminLoadAwsCredentialsCmd)
+	adminCmd.AddCommand(adminLoadAwsCredentialsCmd)
 }
--- pkg/admin_load_aws_credentials.go
+++ pkg/admin_load_aws_credentials.go
@@ -2,46 +2,68 @@ package pkg
 
 import (
 	"fmt"
+	"github.com/go-jose/go-jose/v4/json"
 	"io"
 	"net/http"
+	"os"
+	"os/exec"
 
 	"github.com/qovery/qovery-cli/utils"
 )
 
+type AwsStsCredentials struct {
+	AccessKeyId     string `json:"access_key_id"`
+	SecretAccessKey string `json:"secret_access_key"`
+	SessionToken    string `json:"session_token"`
+}
+
 func LoadAwsCredentials(roleArn string) error {
-	awsCredentials, err := fetchAwsCredentials(roleArn)
-	println(awsCredentials)
+	awsStsCredentialsBody, err := fetchAwsCredentials(roleArn)
+	utils.CheckError(err)
+
+	awsStsCredentials := AwsStsCredentials{}
+	err = json.Unmarshal(awsStsCredentialsBody, &awsStsCredentials)
+	utils.CheckError(err)
+
+	// Set the environment variables for child processes
+	os.Setenv("AWS_ACCESS_KEY_ID", awsStsCredentials.AccessKeyId)
+	os.Setenv("AWS_SECRET_ACCESS_KEY", awsStsCredentials.SecretAccessKey)
+	os.Setenv("AWS_SESSION_TOKEN", awsStsCredentials.SessionToken)
+	utils.PrintlnInfo("AWS credentials loaded successfully in current environment for child process. (AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_SESSION_TOKEN)")
+
+	// Get the user's default shell
+	shell := os.Getenv("SHELL")
+	if shell == "" {
+		shell = "/bin/bash" // Default to bash if SHELL is not set
+	}
+	// Launch the shell
+	utils.PrintlnInfo("Launching new shell with AWS credentials...")
+	cmd := exec.Command(shell)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err = cmd.Run()
 	if err != nil {
-		return err
+		return fmt.Errorf("error launching shell: %v", err)
 	}
 	return nil
 }
 
-func fetchAwsCredentials(roleArn string) (string, error) {
+func fetchAwsCredentials(roleArn string) ([]byte, error) {
 	tokenType, token, err := utils.GetAccessToken()
-	if err != nil {
-		return "", err
-	}
+	utils.CheckError(err)
 
 	req, err := http.NewRequest(http.MethodPost, utils.GetAdminUrl()+"/aws/credentials/assume-role?role_arn="+roleArn, nil)
-	if err != nil {
-		return "", err
-	}
+	utils.CheckError(err)
 	req.Header.Set("Authorization", utils.GetAuthorizationHeaderValue(tokenType, token))
 	req.Header.Set("Content-Type", "application/json")
 
 	res, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return "", err
+	utils.CheckError(err)
+	body, _ := io.ReadAll(res.Body)
+	if res.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("cannot fetch aws credentials (status_code=%d) %s", res.StatusCode, body)
 	}
-	if res.StatusCode != 200 {
-		return "", fmt.Errorf("cannot fetch aws credentials (status_code=%d)", res.StatusCode)
-	}
-
-	bodyBytes, err := io.ReadAll(res.Body)
-	if err != nil {
-		return "", err
-	}
-	defer res.Body.Close()
-	return string(bodyBytes), nil
+	utils.CheckError(err)
+	return body, nil
 }
